8e5fb6b58fedfba8ab76d3eb5f32f027f6faabbc
Tayyab Hanif <mrtayyabhanif@gmail.com>
2017-06-01 10:27:09 +0500

    Yahoooo!

---
+++ js/customizer.js
@@ -0,0 +1,54 @@
+/**
+ * File customizer.js.
+ *
+ * Theme Customizer enhancements for a better user experience.
+ *
+ * Contains handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+
+( function( $ ) {
+
+	// Site title and description.
+	wp.customize( 'blogname', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-title a' ).text( to );
+		} );
+	} );
+	wp.customize( 'blogdescription', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-description' ).text( to );
+		} );
+	} );
+
+	wp.customize( 'tcx_display_header', function( value ) {
+	value.bind( function( to ) {
+		false === to ? $( '#header' ).hide() : $( '#header' ).show();
+		} );
+	} );
+
+	wp.customize( 'the_leader_link_color', function( value ) {
+		value.bind( function( to ) {
+			$( 'a' ).css( 'color', to );
+		} );
+	} );
+
+	// Header text color.
+	wp.customize( 'header_textcolor', function( value ) {
+		value.bind( function( to ) {
+			if ( 'blank' === to ) {
+				$( '.site-title a, .site-description' ).css( {
+					'clip': 'rect(1px, 1px, 1px, 1px)',
+					'position': 'absolute'
+				} );
+			} else {
+				$( '.site-title a, .site-description' ).css( {
+					'clip': 'auto',
+					'position': 'relative'
+				} );
+				$( '.site-title a, .site-description' ).css( {
+					'color': to
+				} );
+			}
+		} );
+	} );
+} )( jQuery );
