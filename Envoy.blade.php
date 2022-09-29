2eede87e43d58489a697709fd74a56ebca22083b
Tayyab <mrtayyabhanif@gmail.com>
2022-09-30 00:26:45 +0500

    Update Envoy.blade.php

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -19,6 +19,7 @@
 
 @task('clone_repository')
     echo 'Cloning repository'
+    echo {{ $repository }} >> repo.txt
     [ -d {{ $releases_dir }} ] || mkdir {{ $releases_dir }}
     git clone --depth 1 {{ $repository }} {{ $new_release_dir }}
     cd {{ $new_release_dir }}
